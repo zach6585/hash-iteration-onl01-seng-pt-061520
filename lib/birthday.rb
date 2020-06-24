0c4beccc066dc4b0b132634ef5656efa9d6c04de
zachary mines <zmines@wesleyan.edu>
2020-06-24 19:10:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,5 +13,6 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if 
   end
 end
